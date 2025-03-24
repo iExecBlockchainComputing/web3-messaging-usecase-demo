3b97a2c567b41492d2c34a67c51fc60136c1a941
ErwanDecoster <wanerd2@gmail.com>
2025-03-24 16:56:57 +0100

    feat: add onboarding popup with step-by-step guidance and local storage tracking

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { NavBar } from '@/components/navBar/NavBar.tsx';
 import { TopNavBar } from '@/components/navBar/TopNavBar.tsx';
 import { useWatchAccount } from '@/hooks/useWatchAccount.ts';
+import OnboardingPopup from '@/layouts/OnboardingPopup.tsx';
 import useUserStore from '@/stores/useUser.store.ts';
 import { Toaster } from '../components/ui/toaster.tsx';
 import ConnectWallet from './ConnectWallet.tsx';
