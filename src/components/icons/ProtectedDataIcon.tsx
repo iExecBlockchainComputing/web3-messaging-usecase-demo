2ba85f6248a6c42c1968c16087043ce89cca5d0f
ErwanDecoster <wanerd2@gmail.com>
2025-03-26 16:20:44 +0100

    feat: add ProtectedDataIcon component and integrate it into ProtectedDataDetails

---
+++ src/components/icons/ProtectedDataIcon.tsx
@@ -0,0 +1,34 @@
+const ProtectedDataIcon = ({ size = 20, className = '' }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      d="M8.5 16H2.3C1.58203 16 1 14.5449 1 12.75V6.25C1 4.45507 1.58203 3 2.3 3H12.7C13.418 3 14 4.45507 14 6.25V8.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16.8574 13.5005H9.85742C9.30514 13.5005 8.85742 13.9482 8.85742 14.5005V18.0005C8.85742 18.5528 9.30514 19.0005 9.85742 19.0005H16.8574C17.4097 19.0005 17.8574 18.5528 17.8574 18.0005V14.5005C17.8574 13.9482 17.4097 13.5005 16.8574 13.5005Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M10.7861 13.1426V11.2379C10.7861 10.6064 11.0571 10.0008 11.5393 9.5543C12.0215 9.10778 12.6756 8.85693 13.3576 8.85693C14.0395 8.85693 14.6936 9.10778 15.1758 9.5543C15.6581 10.0008 15.929 10.6064 15.929 11.2379V13.1426"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ProtectedDataIcon;
