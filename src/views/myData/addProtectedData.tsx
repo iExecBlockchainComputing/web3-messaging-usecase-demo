5b94b642eeb8a891f38099a6f1b46d8618c0a4ac
ErwanDecoster <wanerd2@gmail.com>
2025-03-27 15:20:13 +0100

    fix: update placeholder text in AddProtectedData component

--- src/views/myData/addProtectedData.tsx
+++ src/views/myData/addProtectedData.tsx
@@ -298,8 +298,8 @@ export default function AddProtectedData() {
                   }}
                   placeholder={
                     formData.dataType === 'mail'
-                      ? 'Ex: Frank Wallet email address'
-                      : 'Ex: Frank Wallet telegram'
+                      ? 'Ex: Antoine Wallet email address'
+                      : 'Ex: Antoine Wallet telegram'
                   }
                   className="focus:border-primary border-grey-300 mt-2 max-w-xl rounded-lg border bg-transparent p-3 transition duration-200 focus:outline-none"
                   type="text"
