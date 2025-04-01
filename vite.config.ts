b6d54a606d421d25ba2644625512d1e41656489b
ErwanDecoster <wanerd2@gmail.com>
2025-04-01 15:47:17 +0200

    Log environment variable for production base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   // In prod, app will be served under `demo.iex.ec/web3messaging` path
+  console.log(env.SET_SUBPATH_FOR_PROD);
+  
   const basePath =
     env.SET_SUBPATH_FOR_PROD === 'true' ? '/web3messaging' : '/';
   console.log('[vite] Building with base path:', basePath);
