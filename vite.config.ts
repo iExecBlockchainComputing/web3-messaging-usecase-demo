07c9871e06846aa7fabf2d911f515bdd3b38ff09
ErwanDecoster <wanerd2@gmail.com>
2025-04-01 15:49:58 +0200

    Fix loadEnv call in Vite configuration to ensure proper environment variable loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '');
   // In prod, app will be served under `demo.iex.ec/web3messaging` path
   console.log(env.SET_SUBPATH_FOR_PROD);
   
