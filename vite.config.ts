b66ca08bf13cfccca27642b4cee4b74dbe230423
ErwanDecoster <wanerd2@gmail.com>
2025-04-03 17:18:45 +0200

    fix: set base path for production and development in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  // In prod, app will be served under `demo.iex.ec/web3messaging` path
-  const basePath =
-    env.SET_SUBPATH_FOR_PROD === 'true' ? '/web3messaging' : '/';
-  console.log('[vite] Building with base path:', basePath);
   return {
-    base: basePath,
+    base: "/web3messaging",
     define: {
       'process.env.NEXT_PUBLIC_SECURE_SITE_ORIGIN': JSON.stringify(
         env.NEXT_PUBLIC_SECURE_SITE_ORIGIN || 'https://secure.walletconnect.org'
